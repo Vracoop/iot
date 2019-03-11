74af29454eda4b8fb7267b5eac1589e780feb025
Vincent Van Rossem <vvrossem@gmail.com>
2019-03-11 10:23:16 +0100

    [FIX] correct license

--- toledo_scale/controllers/__init__.py
+++ toledo_scale/controllers/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import main
